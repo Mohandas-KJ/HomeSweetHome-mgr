f18201621018fd183777cd1577b8dd6d340832da
Mohan Das <mohandaskj.11@gmail.com>
2026-03-01 13:30:02 +0530

    Refactor banner script to launch in xfce4-terminal and add systemd service files for EFI manager

--- agent/banner.sh
+++ agent/banner.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-figlet "HoSweetHo" | lolcat
-echo "Next Boot Configured"
-echo "Ready"
+xfce4-terminal --command="bash -c '
+figlet HoSweetHo | lolcat
+echo Next Boot Configured
+echo Ready
 sleep 4
+' "
